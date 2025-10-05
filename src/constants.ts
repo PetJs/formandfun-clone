2a7184b0c8d0ad98e57b943b5100b75d288eed11
PetJs <peterfagoroye76@gmail.com>
2025-10-05 22:57:42 +0100

    feat: Modifies constants file

--- src/constants.ts
+++ src/constants.ts
@@ -3,8 +3,8 @@ export const gif = [
     "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759639587/ILLpcsHOAB_576p_1744824343_vu4fee.mp4",
     "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642078/bheeicedz8a5tcbo6sr84repcz7zjtyfocua_zaf61j.mp4",
     "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759639587/GWazvydWJs_576p_1744824344_wyftge.mp4",
-    "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759639607/LCVDnqXAon_576p_1751149542_a65jqv.mp4",
-    "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759639607/LCVDnqXAon_576p_1751149542_a65jqv.mp4",
+    "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759639606/bheeiced5jnufmkv27k6gsr8vdhpjn2romax_idegjm.mp4",
+    "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759639607/LCVDnqXAon_576p_1751149542_a65jqv.mp4"
 ]
 
 
@@ -157,3 +157,66 @@ export const media = [
         description: "A cool bottle",
     },
 ]
+
+export const brands = [
+  {
+    logos: [
+        {
+            black: "https://res.cloudinary.com/dm7vlpslq/image/upload/v1759697491/67db30e3e358457209bcadb8_logo_Doordash_fmombo.svg",
+            white: "https://res.cloudinary.com/dm7vlpslq/image/upload/v1759697490/67db30e3a04d22ccb8a4055d_logo_Doordash--white_cc1v8a.svg" ,
+            bg: "#EA1D2C",
+        },
+        {
+            black: "https://res.cloudinary.com/dm7vlpslq/image/upload/v1759697491/67ddd9d69b96eb906bd561ef_logo_Spotify_yggdru.svg",
+            white: "https://res.cloudinary.com/dm7vlpslq/image/upload/v1759697492/67ddd9d6664033ef9d0a2c5a_logo_Spotify--white_a0uoge.svg" ,
+            bg: "#7ce21cff",
+        },
+    ]
+  },
+  {
+    logos: [
+        {
+            black: "https://res.cloudinary.com/dm7vlpslq/image/upload/v1759697492/67db30e34e47ff0775e1c2b8_logo_Powerade_or6jql.svg",
+            white: "https://res.cloudinary.com/dm7vlpslq/image/upload/v1759697492/67db30e22a8a2708eb8d48c6_logo_Powerade--white_kfylds.svg" ,
+            bg: "#EA1D2C",
+        },
+        {
+            black: "https://res.cloudinary.com/dm7vlpslq/image/upload/v1759697491/67db30e44d76c91cbd0a8b60_logo_Oreo_dyfggv.svg",
+            white: "https://res.cloudinary.com/dm7vlpslq/image/upload/v1759697490/67db30e38dcb3c792378fcf3_logo_Oreo-white_uhbtrx.svg" ,
+            bg: "#1CABE2",
+        },
+    ]
+  },
+  {
+    logos: [
+        {
+            black: "https://res.cloudinary.com/dm7vlpslq/image/upload/v1759697490/67db30e26165aba082f8a5f2_logo_Google_kgo97l.svg",
+            white: "https://res.cloudinary.com/dm7vlpslq/image/upload/v1759697490/67db30e22e4501b9a0d48590_logo_Google--white_ok1zek.svg" ,
+            bg: "#EA1D2C",
+        },
+        {
+            black: "https://res.cloudinary.com/dm7vlpslq/image/upload/v1759697489/67db30e35934a71f11cce411_logo_Ogilvy_xaeufp.svg",
+            white: "https://res.cloudinary.com/dm7vlpslq/image/upload/v1759697489/67db30e3d3b7ed6b7d1e8d93_logo_Ogilvy--white_yhn9tg.svg" ,
+            bg: "#1CABE2",
+        },
+    ]
+  },
+  {
+    logos: [
+        {
+            black: "https://res.cloudinary.com/dm7vlpslq/image/upload/v1759697489/67db30e30170e1fc0f157f58_logo_Coca-Cola_gxf8mk.svg",
+            white: "https://res.cloudinary.com/dm7vlpslq/image/upload/v1759697489/67db30e31064fa76ec051bcf_logo_Coca-Cola--white_kuhfw6.svg" ,
+            bg: "#EA1D2C",
+        },
+        {
+            black: "https://res.cloudinary.com/dm7vlpslq/image/upload/v1759697488/67db30e4d04280190712544d_logo_Saatchi-Saatchi_cx2bw8.svg",
+            white: "https://res.cloudinary.com/dm7vlpslq/image/upload/v1759697488/67db30e44a10d1c4f3125118_logo_Saatchi-Saatchi--white_kggftb.svg" ,
+            bg: "#000",
+        },
+    ]
+  },
+];
+
+
+
+
