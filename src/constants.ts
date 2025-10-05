b222f86ec592ddb40fd006ec249a1852fc690c7f
PetJs <peterfagoroye76@gmail.com>
2025-10-05 07:26:17 +0100

    feat: Refactor constants

---
+++ src/constants.ts
@@ -0,0 +1,159 @@
+export const gif = [
+    "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642111/bheeicedy3ihyzyn31iuum3m4cldsk8dqlxe_wh4lip.mp4",
+    "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759639587/ILLpcsHOAB_576p_1744824343_vu4fee.mp4",
+    "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642078/bheeicedz8a5tcbo6sr84repcz7zjtyfocua_zaf61j.mp4",
+    "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759639587/GWazvydWJs_576p_1744824344_wyftge.mp4",
+    "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759639607/LCVDnqXAon_576p_1751149542_a65jqv.mp4",
+    "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759639607/LCVDnqXAon_576p_1751149542_a65jqv.mp4",
+]
+
+
+
+
+export const awards = [
+    {
+        id: 0o1,
+        title: "Awwwards",
+        project: "Form&Fun Portfolio",
+        category: "AI, Immersive & Games",
+        year: "2025",
+    },
+    {
+        id: 0o2,
+        title: "Webby Awards",
+        project: "Powerade Mind Zone",
+        category: "AI, Immersive & Games",
+        year: "2025",
+    },
+    {
+        id: 0o3,
+        title: "Clio Awards-Gold",
+        project: "Oreo & Pacman Supermarcade",
+        category: "Interactive/Experimental",
+        year: "2025",
+    },
+    {
+        id: 0o4,
+        title: "3xClio Awards-Shortlist",
+        project: "Oreo & Pacman Supermarcade",
+        category: "1x Entertainment",
+        year: "2025",
+    },
+    {
+        id: 0o5,
+        title: "Auggie Award",
+        project: "Oreo & Pacman Supermarcade",
+        category: "Best Campaign",
+        year: "2025",
+    },
+    {
+        id: 0o6,
+        title: "3x ADC Festival - Silver",
+        project: "Oreo & Pacman Supermarcade",
+        category: "1x Craft AR/VR",
+        year: "2025",
+    },
+    {
+        id: 0o7,
+        title: "Clio Awards-Silver",
+        project: "Powerrade Mind Zone",
+        category: "Digital/Mobile",
+        year: "2024",
+    },
+    {
+        id: 8,
+        title: "Cannes Lions - Bronze",
+        project: "Oreo & Pacman Supermarcade",
+        category: "Outdoor",
+        year: "2025",
+    },
+    {
+        id: 9,
+        title: "4x Cannes Lions - Shortlist",
+        project: "Oreo & Pacman Supermarcade",
+        category: "Outdoor",
+        year: "2024",
+    },
+    {
+        id: 10,
+        title: "New York Festivals - Bronze",
+        project: "Oreo & Pacman Supermarcade",
+        category: "Digital/Mobile: Best Use",
+        year: "2024",
+    },
+]
+
+
+export const expertise = [
+    {
+        image: "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642101/bhefcffg2eta2ry3k0f67vgol44t62chntda_hksyby.mp4",
+        bgColorHover: "hover:bg-blue-200",
+        title: "Technology",
+        leftList: [
+            "Spatial Computing (AR, VR, XR)",
+            "AI Tools & Experiences",
+            "Web Development",
+            "WebGL Experiences"
+        ],
+        rightList: [
+            "Web3/Blockchain",
+            "Game Development",
+            "Rapid Prototyping",
+        ]
+    },
+    {
+        image: "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642090/bheeicecyrhzyi3dd26lfivkab97k907q706_qzzyps.mp4",
+        bgColorHover: "hover:bg-purple-200",
+        title: "Technology",
+        leftList: [
+            "Spatial Computing (AR, VR, XR)",
+            "AI Tools & Experiences",
+            "Web Development",
+            "WebGL Experiences"
+        ],
+        rightList: [
+            "Web3/Blockchain",
+            "Game Development",
+            "Rapid Prototyping",
+        ]
+    },
+    {
+        image: "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642090/bheeiceccip4dtalzn188e4w87papbecrhou_an9son.mp4",
+        bgColorHover: "hover:bg-green-200",
+        title: "Technology",
+        leftList: [
+            "Spatial Computing (AR, VR, XR)",
+            "AI Tools & Experiences",
+            "Web Development",
+            "WebGL Experiences"
+        ],
+        rightList: [
+            "Web3/Blockchain",
+            "Game Development",
+            "Rapid Prototyping",
+        ]
+    },
+]
+
+export const media = [
+    {
+        video: "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642096/bheeicdi21w0hkxyqiby6vxovghp64qh2wan_qwmt7l.mp4",
+        title: "Bottle",
+        description: "A cool bottle",
+    },
+    {
+        video: "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642676/YThAIwoXKB_576p_1744824072_ccmg6f.mp4",
+        title: "Bottle",
+        description: "A cool bottle",
+    },
+    {
+        video: "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642080/bheeicea8cwvkf21vcnbg8jhx3wo0wyb42im_q9yvuz.mp4",
+        title: "Bottle",
+        description: "A cool bottle",
+    },
+    {
+        video: "https://res.cloudinary.com/dm7vlpslq/video/upload/v1759642089/bheeicea3wowr2ifyqckn5737cnu42mewpr1_a8wqzo.mp4",
+        title: "Bottle",
+        description: "A cool bottle",
+    },
+]
